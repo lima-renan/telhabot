72ca391081ed210827751103e4a93f645bfa2231
Renan Lima <renan.flimabr@gmail.com>
2021-08-24 20:49:49 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # telhabot
 Telegram bot in Haskell
+ Instalar o Postgresql (sudo apt install postgresql postgresql-contrib), criar o usuário star (sudo -u postgres psql
+ create database star;) e a senha wader (create user star with encrypted password 'wader';)para o dataset habot (grant all privileges on database habot to star;), utilizar a porta 5432
--- src/Schema.hs
+++ src/Schema.hs
@@ -16,12 +16,218 @@
 
 module Schema where
 
+
+
+
+
 import Control.Monad.IO.Class
-import qualified Data.Text as T
-import Data.Time
+
+
+import Database.Esqueleto.PostgreSQL.JSON
 import Database.Persist.Sql
 import Database.Persist.TH
+
+
+
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.Types (Parser)
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as B
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Time (UTCTime)
 import GHC.Generics (Generic)
+import Network.HTTP.Conduit 
+import Network.HTTP.Simple  
+import Prelude.Compat.Repl ( Show, Int, Maybe, IO, FilePath )
+import System.IO
+import UnliftIO (liftIO)
+import UnliftIO.Concurrent
+
+
+
+{- 
+{
+  "data":[
+    {
+      "uid":35,
+      "uf":"SP",
+      "state":"São Paulo",
+      "cases":4164587,
+      "deaths":142528,
+      "suspects":5334,
+      "refuses":596,
+      "datetime":"2021-08-14T22:34:41.818Z"
+    },
+    {
+      "uid":31,
+      "uf":"MG",
+      "state":"Minas Gerais",
+      "cases":2019435,
+      "deaths":51849,
+      "suspects":925,
+      "refuses":104,
+      "datetime":"2021-08-14T22:34:41.818Z"
+    },
+    {
+      "uid":41,
+      "uf":"PR",
+      "state":"Paraná",
+      "cases":1414605,
+      "deaths":36432,
+      "suspects":400,
+      "refuses":119,
+      "datetime":"2021-08-14T22:34:41.818Z"
+    },
+  ]
+}
+ -}
+
+data State = State {
+    uid       :: !(Maybe Int)
+  , uf        :: !(Maybe T.Text)
+  , state     :: !(Maybe T.Text)
+  , cases     :: !(Maybe Int)
+  , deaths    :: !(Maybe Int)
+  , suspects  :: !(Maybe Int)
+  , refuses   :: !(Maybe Int)
+  , updatedAt :: !(Maybe UTCTime)
+} deriving Show
+
+-- para um único estado
+instance FromJSON State where
+  parseJSON (Object v) = State
+                         <$> (v .: "uid")
+                         <*> (v .: "uf")
+                         <*> (v .: "state")
+                         <*> (v .: "cases")
+                         <*> (v .: "deaths")
+                         <*> (v .: "suspects")
+                         <*> (v .: "refuses")
+                         <*> (v .: "datetime")
+  parseJSON _ = mzero
+
+newtype States = States { 
+  states :: [State] 
+} deriving Show
+
+                     
+instance FromJSON States where
+    parseJSON (Object v) = do
+        ms <- v .: "data"
+        States <$> parseJSON ms
+    parseJSON _ = mzero
+
+
+{- maxStates :: [State] -> [Int]
+maxStates xs = -}
+
+
+-- JSON dos paises - COVID - Amostra
+
+{- 
+{
+  "data": [
+    {
+      "country": "Canada",
+      "cases": 1299,
+      "confirmed": 1328,
+      "deaths": 19,
+      "recovered": 10,
+      "updated_at": "2020-03-21T23:43:02.000Z"
+    },
+    {
+      "country": "Maldives",
+      "cases": 13,
+      "confirmed": 13,
+      "deaths": 0,
+      "recovered": 0,
+      "updated_at": "2020-03-15T18:20:18.000Z"
+    },
+    {
+      "country": "Lithuania",
+      "cases": 97,
+      "confirmed": 99,
+      "deaths": 1,
+      "recovered": 1,
+      "updated_at": "2020-03-22T02:13:18.000Z"
+    }
+  ]
+}
+ -}
+
+data Country = Country {
+    country          :: !(Maybe T.Text)
+  , casesCountry     :: !(Maybe Int)
+  , confirmed        :: !(Maybe Int)
+  , deathsCountry    :: !(Maybe Int)
+  , recovered        :: !(Maybe Int)
+  , updatedAtCountry :: !(Maybe UTCTime)
+} deriving Show
+
+-- para um único país
+instance FromJSON Country where
+  parseJSON (Object v) = Country
+                         <$> (v .: "country")
+                         <*> (v .: "cases")
+                         <*> (v .: "confirmed")
+                         <*> (v .: "deaths")
+                         <*> (v .: "recovered")
+                         <*> (v .: "updated_at")
+  parseJSON _ = mzero
+
+-- para vários países
+newtype Countries = Countries { 
+  countries :: [Country] 
+} deriving Show
+
+                     
+instance FromJSON Countries where
+  parseJSON (Object v) = do
+      ms <- v .: "data"
+      Countries <$> parseJSON ms
+  parseJSON _ = mzero
+
+{- instance ToJSON Country where
+ toJSON (Country country cases confirmed deaths recovered updated_at) =
+    object [ "country"  .= country
+           , "cases"   .= cases
+           , "confirmed"       .= confirmed
+           , "deaths" .= deaths
+           , "recovered" .= recovered 
+           , "updated_at" .= updated_at
+             ]  -}
+
+-- JSON nome de países - amostra
+{- [ { "gentilico" : "afegãne",
+    "nome_pais" : "Afeganistão",
+    "nome_pais_int" : "Afghanistan",
+    "sigla" : "AF"
+  },
+  { "gentilico" : "sul-africana",
+    "nome_pais" : "África do Sul",
+    "nome_pais_int" : "South Africa",
+    "sigla" : "ZA"
+  },
+  { "gentilico" : "albanesa",
+    "nome_pais" : "Albânia",
+    "nome_pais_int" : "Albania",
+    "sigla" : "AL"
+  }
+] -}
+
+data CountriesN = CountriesN{
+   gentilico      :: T.Text 
+ , nome_pais      :: T.Text 
+ , nome_pais_int  :: T.Text 
+ , sigla          :: T.Text
+
+}deriving (Show, Generic)
+
+instance FromJSON CountriesN
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 
@@ -87,29 +293,3 @@ share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 doMigration :: MonadIO m => SqlPersistT m ()
 doMigration = runMigration migrateAll
 
-data State = State {
-    uid       :: !(Maybe Int)
-  , uf        :: !(Maybe T.Text)
-  , state     :: !(Maybe T.Text)
-  , cases     :: !(Maybe Int)
-  , deaths    :: !(Maybe Int)
-  , suspects  :: !(Maybe Int)
-  , refuses   :: !(Maybe Int)
-  , updatedAt :: !(Maybe UTCTime)
-} deriving Show
-
-data Country = Country {
-    country          :: !(Maybe T.Text)
-  , casesCountry     :: !(Maybe Int)
-  , confirmed        :: !(Maybe Int)
-  , deathsCountry    :: !(Maybe Int)
-  , recovered        :: !(Maybe Int)
-  , updatedAtCountry :: !(Maybe UTCTime)
-} deriving Show
-
-data CountriesN = CountriesN{
-   gentilico      :: T.Text 
- , nome_pais      :: T.Text 
- , nome_pais_int  :: T.Text 
- , sigla          :: T.Text
-}deriving (Show, Generic)
